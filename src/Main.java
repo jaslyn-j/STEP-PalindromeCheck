1807651f8974ab3cb7cdcef1a7354ef35d13f411
jaslyn-j <jaslynjacob026@gmail.com>
2026-02-18 15:17:31 +0530

    UC4: using char array

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,29 @@
 import java.util.*;
 
 /* @author: Jaslyn Jacob
-   @version: 3.0
+   @version: 4.0
  */
 
 class Main{
     public static void main(String[] args){
         Scanner input=new Scanner(System.in);
-        double version=3.0;
-        System.out.println("Welcome to the Palindrome Checker App (UC3) ");
+        double version=4.0;
+        System.out.println("Welcome to the Palindrome Checker App (UC4) ");
         System.out.println("Version: "+version);
 
         System.out.print("Enter a word to check: ");
         String word=input.nextLine();
 
         String lowerWord=word.toLowerCase();
-        String reversed= "";
-        for(int i=word.length()-1; i>=0; i--)
-            reversed+=(lowerWord.charAt(i));
-
-        boolean palindrome;
-        if (lowerWord.equals(reversed.toString())) palindrome = true;
-        else palindrome = false;
+        char[] charArr=lowerWord.toCharArray();
+        boolean palindrome=true;
+        int start=0, end=charArr.length -1;
+        while(start<end){
+             if(charArr[start]!=charArr[end]){
+                 palindrome=false;
+                 break;
+             }start++; end--;
+         }
 
         System.out.println("Input text: "+word);
         System.out.println("Is it a palindrome? : "+palindrome);
