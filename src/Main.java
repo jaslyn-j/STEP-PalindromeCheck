0008443ac973bcbd1ace2e621be25b159a74d481
jaslyn-j <jaslynjacob026@gmail.com>
2026-02-18 15:27:20 +0530

    UC5: using stack

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
 import java.util.*;
 
 /* @author: Jaslyn Jacob
-   @version: 4.0
+   @version: 5.0
  */
 
 class Main{
     public static void main(String[] args){
         Scanner input=new Scanner(System.in);
-        double version=4.0;
-        System.out.println("Welcome to the Palindrome Checker App (UC4) ");
+        double version=5.0;
+        System.out.println("Welcome to the Palindrome Checker App (UC5) ");
         System.out.println("Version: "+version);
 
         System.out.print("Enter a word to check: ");
@@ -17,13 +17,18 @@ class Main{
         String lowerWord=word.toLowerCase();
         char[] charArr=lowerWord.toCharArray();
         boolean palindrome=true;
-        int start=0, end=charArr.length -1;
-        while(start<end){
-             if(charArr[start]!=charArr[end]){
-                 palindrome=false;
-                 break;
-             }start++; end--;
-         }
+        int i=0;
+        Stack<Character> charStack=new Stack<>();
+
+        for(char c: charArr){
+            charStack.push(c);
+        }
+        while(!charStack.isEmpty()){
+            if(charStack.pop() != charArr[i]){
+                palindrome=false;
+                break;
+            }i++;
+        }
 
         System.out.println("Input text: "+word);
         System.out.println("Is it a palindrome? : "+palindrome);
