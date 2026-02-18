4a152fe8e59c0552bcbe97e653531b1aced6aa9d
jaslyn-j <jaslynjacob026@gmail.com>
2026-02-18 14:17:50 +0530

    uc1 completed

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+/* @author: Jaslyn Jacob
+   @version: 1.0
+ */
+
+class Main{
+    public static void main(String[] args){
+        double version=1.0;
+        System.out.println("Welcome to the Palindrome Checker App (UC1) ");
+        System.out.println("Version: "+version);
+    }
+}
