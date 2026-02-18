4edc4073a2cb9131107302fcaeda5c10608a9d65
jaslyn-j <jaslynjacob026@gmail.com>
2026-02-18 14:50:17 +0530

    uc2 completed:hardcoded palindrome

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,26 @@
 import java.util.*;
 
 /* @author: Jaslyn Jacob
-   @version: 1.0
+   @version: 2.0
  */
 
 class Main{
     public static void main(String[] args){
-        double version=1.0;
+        double version=2.0;
         System.out.println("Welcome to the Palindrome Checker App (UC1) ");
         System.out.println("Version: "+version);
+
+        String word="Race Bar";
+        String lowerWord=word.toLowerCase();
+        String reversed= "";
+        for(int i=word.length()-1; i>=0; i--)
+            reversed+=(lowerWord.charAt(i));
+
+        boolean palindrome;
+        if (lowerWord.equals(reversed.toString())) palindrome = true;
+        else palindrome = false;
+
+        System.out.println("Input text: "+word);
+        System.out.println("Is it a palindrome? : "+palindrome);
     }
 }
