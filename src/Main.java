d91ee540c117a3762a7ca4416391207dd01afac8
jaslyn-j <jaslynjacob026@gmail.com>
2026-02-25 15:49:57 +0530

    UC10: space insensitive palindrome check

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,28 @@
 import java.util.*;
 
 /* @author: Jaslyn Jacob
-   @version: 9.0
+   @version: 10.0
  */
 class Main{
-    public static boolean check(String s, int start, int end){
-        if(start>=end) return true;
-        if(s.charAt(start) != s.charAt(end)){
-            return false;
-        }
-        return check(s, start+1, end-1);
-    }
-
     public static void main(String[] args){
         Scanner input=new Scanner(System.in);
-        double version=9.0;
-        System.out.println("Welcome to the Palindrome Checker App (UC9) ");
+        double version=10.0;
+        System.out.println("Welcome to the Palindrome Checker App (UC10) ");
         System.out.println("Version: "+version);
 
         System.out.print("Enter a word to check: ");
         String word=input.nextLine();
 
         String lowerWord=word.toLowerCase();
+        String normalizedWord=lowerWord.replaceAll("\\s", "");
         boolean palindrome=true;
 
-        palindrome=check(lowerWord, 0, lowerWord.length()-1);
+        for(int i=0; i<normalizedWord.length()/2; i++){
+            if(normalizedWord.charAt(i) != normalizedWord.charAt(normalizedWord.length()-i-1)){
+                palindrome=false;
+                break;
+            }
+        }
 
         System.out.println("Input text: "+word);
         System.out.println("Is it a palindrome? : "+palindrome);
